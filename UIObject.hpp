8b6d131ba1aa0b7a2ef450c0b7038ec549fb7606
Sander Jochems <sanderjochems@hotmail.nl>
2021-09-14 16:34:13 +0200

    Add constructor to UIObject
    Co-Authored-By: daanh432 <8281824+daanh432@users.noreply.github.com>

--- UIObject.hpp
+++ UIObject.hpp
@@ -9,6 +9,17 @@ namespace spic {
      * @brief Base class for a user interface object like Button or Text.
      */
     class UIObject : public GameObject {
+        public:
+            /**
+             * @brief Constructor.
+             * @param name The name for the game object.
+             * @param tag The tag for the game object.
+             * @param layer The layer for the game object.
+             * @param width The width of the UI object.
+             * @param height The height of the UI object.
+             */
+            UIObject(const std::string& name, const std::string& tag, const int layer, const double width, const double height);
+
         private:
             double width;
             double height;
