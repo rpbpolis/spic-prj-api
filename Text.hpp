d799a27896e1c7fc32939e605a02903c820f0765
Sander Jochems <sanderjochems@hotmail.nl>
2021-09-14 16:33:45 +0200

    Add constructor to Text
    Co-Authored-By: daanh432 <8281824+daanh432@users.noreply.github.com>

--- Text.hpp
+++ Text.hpp
@@ -20,6 +20,31 @@ namespace spic {
      * @brief Class representing a piece of text which can be rendered.
      */
     class Text : public UIObject {
+        public:
+            /**
+             * @brief Constructor.
+             * @param name The name for the game object.
+             * @param tag The tag for the game object.
+             * @param layer The layer for the game object.
+             * @param width The width of the UI object.
+             * @param height The height of the UI object.
+             */
+            Text(const std::string& name, const std::string& tag, const int layer, const double width, const double height);
+            
+            /**
+             * @brief Constructor.
+             * @param name The name for the game object.
+             * @param tag The tag for the game object.
+             * @param layer The layer for the game object.
+             * @param width The width of the UI object.
+             * @param height The height of the UI object.
+             * @param text The content of the Text object
+             * @param font The font the Text object will use to render
+             * @param size The size the font will be rendered at
+             * @param alignment The alignment the content will be rendered with 
+             * @param color The color the content will be rendered in
+             */
+            Text(const std::string& name, const std::string& tag, const int layer, const double width, const double height, const std::string& text, const std::string& font, const int size, const Alignment alignment, const Color& color);
         private:
             std::string text;
             std::string font;
