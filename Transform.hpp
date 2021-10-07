e44300675fe3c448ed54f695f1879ea4f8d96d6c
SamVerburg <sam.verburg@hotmail.com>
2021-10-07 12:36:28 +0200

    updated comments

--- Transform.hpp
+++ Transform.hpp
@@ -8,9 +8,9 @@ namespace spic {
      * @brief Instances of this class represent specific 2D transformations.
      */
     struct Transform {
-        Point position; // Translation (shift).
-        double rotation; // Rotation, in radians.
-        double scale; // multiplication factor
+        Point position; // Translation (shift)
+        double rotation; // Rotation, in radians
+        double scale; // Multiplication factor
     };
 }
 
