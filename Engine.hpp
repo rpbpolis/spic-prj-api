8f4478cf3cfa992e3e2928907a32ea8f61ee9d3f
JRaams <47593569+JRaams@users.noreply.github.com>
2021-10-12 14:20:21 +0200

    Add Engine.hpp

---
+++ Engine.hpp
@@ -0,0 +1,33 @@
+#ifndef SPIC_PRJ_API_CD_ENGINE_HPP
+#define SPIC_PRJ_API_CD_ENGINE_HPP
+
+#include "Scene.hpp"
+#include <iostream>
+#include <stack>
+
+namespace spic {
+    class Engine {
+    private:
+        static Engine instance;
+        Engine() = default;
+
+        std::stack<std::shared_ptr<Scene>> scenes;
+
+    public:
+        static Engine& Instance();
+        Engine(const Engine&) = delete;
+        Engine& operator=(const Engine&) = delete;
+        Engine(const Engine&&) = delete;
+        Engine& operator=(Engine&&) = delete;
+
+        void Start();
+        void UpdateGameObjects();
+        void RenderActiveScene();
+        void PopScene();
+        std::shared_ptr<Scene> ActiveScene();
+        void ActiveScene(const std::shared_ptr<Scene>& scene);
+        void Shutdown();
+    };
+}
+
+#endif //SPIC_PRJ_API_CD_ENGINE_HPP
