45a575141ee9660674609c49811e44f92d3310b4
rpbpolis <rpbpolis@users.noreply.github.com>
2021-11-09 16:31:08 +0000

    deploy: 51df52fe0173f0322a86b43b79ef02e0c500f3a3

--- search/all_a.js
+++ search/all_a.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['magenta_51',['magenta',['../classspic_1_1Color.html#ad1de7fbd69c3b7d60c0b6435bcae57ea',1,'spic::Color']]],
   ['mousebutton_52',['MouseButton',['../namespacespic_1_1Input.html#aedea49171a9999a91014688dd9c29b04',1,'spic::Input']]],
-  ['mouseposition_53',['MousePosition',['../namespacespic_1_1Input.html#a08e769bec4fdee3ded3bc1571330228a',1,'spic::Input']]]
+  ['mouseposition_53',['MousePosition',['../namespacespic_1_1Input.html#a636cc36a34ae2f391fe5c1efafdd6c12',1,'spic::Input']]]
 ];
