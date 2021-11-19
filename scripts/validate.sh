58a3798d9341937d068b7df5a7a21bbc9075963e
Steven Liebregt <s.liebregt@outlook.com>
2021-11-19 20:15:03 +0100

    disable script

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -6,13 +6,15 @@ readonly CHECK_COMMAND="g++ -fsyntax-only -Wall -Wextra -Wno-deprecated -std=c++
 function main() {
   exitcode=0
 
-  if ! eval "$CHECK_COMMAND Api.hpp"; then
-    exitcode=1
-  fi
+#  if ! eval "$CHECK_COMMAND Api.hpp"; then
+#    exitcode=1
+#  fi
+#
+#  if [[ $exitcode != 0 ]]; then
+#    echo "There are some problems!"
+#  fi
 
-  if [[ $exitcode != 0 ]]; then
-    echo "There are some problems!"
-  fi
+  echo "Don't check"
 
   exit $exitcode
 }
