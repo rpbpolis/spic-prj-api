ee3ca6bbc0eae93c35fe463ac1beadfb720bdb97
Steven Liebregt <s.liebregt@outlook.com>
2021-11-30 15:43:00 +0100

    update

--- IO.hpp
+++ IO.hpp
@@ -6,10 +6,28 @@
 #include <string>
 
 namespace spic::io {
+    /**
+     * Check if a directory exists.
+     *
+     * @param path The path of the directory.
+     * @return TRUE if the directory exists, FALSE if not.
+     */
     bool DirectoryExists(const std::string& path);
 
+    /**
+     * Create a directory.
+     *
+     * @param path The path of the directory.
+     * @return TRUE if the creation was successful, FALSE if not.
+     */
     bool CreateDirectory(const std::string& path);
 
+    /**
+     * Check if a file exists.
+     *
+     * @param path The path of the file.
+     * @return TRUE if the file exists, FALSE if not.
+     */
     bool FileExists(const std::string& path);
 
     /**
@@ -21,6 +39,13 @@ namespace spic::io {
      */
     void OpenFileForWriting(const std::string& path, const std::function<void(std::ostream&)>& writingFn);
 
+    /**
+     * Open the file for reading.
+     *
+     * @param path The path of the file.
+     * @param readingFn A function to be called with the istream of the file, in which you can read the data from the
+     * file.
+     */
     void OpenFileForReading(const std::string& path, const std::function<void(std::istream&)>& readingFn);
 }
 
