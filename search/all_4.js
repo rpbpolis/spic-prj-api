89e810bbcd2637cdb39fbffa2d24aebe597ce43e
rpbpolis <rpbpolis@users.noreply.github.com>
2022-11-09 07:55:23 +0000

    deploy: 57a6901e97d924c5f5f7e057e98bda005ad6e6fc

--- search/all_4.js
+++ search/all_4.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['find_23',['Find',['../classspic_1_1GameObject.html#a468250d6be475c50c0a0466dd2740f24',1,'spic::GameObject']]],
   ['findgameobjectswithtag_24',['FindGameObjectsWithTag',['../classspic_1_1GameObject.html#a8a856ffd67023ae5dd55e2e401684f81',1,'spic::GameObject']]],
-  ['findobjectoftype_25',['FindObjectOfType',['../classspic_1_1GameObject.html#abe9b817a231613c65d08cf59200d5605',1,'spic::GameObject']]],
-  ['findobjectsoftype_26',['FindObjectsOfType',['../classspic_1_1GameObject.html#aa60eac0d485f371b175d26c158c6768e',1,'spic::GameObject']]],
+  ['findobjectoftype_25',['FindObjectOfType',['../classspic_1_1GameObject.html#a39c116d50efe7c3b7eb7be8be161be4c',1,'spic::GameObject']]],
+  ['findobjectsoftype_26',['FindObjectsOfType',['../classspic_1_1GameObject.html#a04cc55e1ade46360ba9eb45a7c499274',1,'spic::GameObject']]],
   ['findwithtag_27',['FindWithTag',['../classspic_1_1GameObject.html#afe1650b6c75098e9b63a0ccece558567',1,'spic::GameObject']]]
 ];
